578a68e13aab2c3a08457d9f90e0fee6c6e71431
saini4 <sainihemantsingh@gmail.com>
2025-01-05 20:15:26 -0800

    Update LongestCommonPrefix.cpp

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -1,3 +1,18 @@
+
+/**
+ * This solution finds the longest common prefix in a list of strings.
+ * The approach starts with the first string in the list and progressively shortens it until it is a common prefix
+ * for all the other strings in the list. It uses the `find()` method to check if the prefix is at the start of each string.
+ * 
+ * Time Complexity:
+ * - O(S), where S is the sum of the lengths of all strings in the list.
+ *   - For each string, in the worst case, we check if the current prefix exists at the start of the string.
+ *   - As the prefix shrinks, the number of comparisons decreases.
+ * 
+ * Space Complexity:
+ * - O(1), not counting the input space, because we only use a few variables to store the prefix and iterate through the list.
+ */
+
 #include <iostream>
 #include <vector>
 #include <string>
